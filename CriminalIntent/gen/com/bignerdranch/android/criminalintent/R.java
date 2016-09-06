4530c48d5f42bd46b1fc82cdc18be3d054c932ee
Nan Li <crscd@crscdtekiiMac.local>
2016-09-06 13:58:22 +0800

    9.ListFragment显示列表

--- CriminalIntent/gen/com/bignerdranch/android/criminalintent/R.java
+++ CriminalIntent/gen/com/bignerdranch/android/criminalintent/R.java
@@ -25,15 +25,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080007;
         public static final int crime_date=0x7f080002;
+        public static final int crime_list_item_dateTextView=0x7f080006;
+        public static final int crime_list_item_solvedCheckBox=0x7f080004;
+        public static final int crime_list_item_titleTextView=0x7f080005;
         public static final int crime_solved=0x7f080003;
         public static final int crime_title=0x7f080001;
         public static final int fragmentContainer=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_crime=0x7f030000;
+        public static final int activity_fragment=0x7f030000;
         public static final int fragment_crime=0x7f030001;
+        public static final int list_item_crime=0x7f030002;
     }
     public static final class menu {
         public static final int crime=0x7f070000;
@@ -45,6 +49,7 @@ public final class R {
         public static final int crime_solved_label=0x7f050005;
         public static final int crime_title_hint=0x7f050002;
         public static final int crime_title_label=0x7f050003;
+        public static final int crimes_title=0x7f050006;
     }
     public static final class style {
         /** 
---
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,26 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+public abstract class SingleFragmentActivity extends FragmentActivity {
+	
+	protected abstract Fragment createFragment();
+
+	@Override
+	protected void onCreate(@Nullable Bundle arg0) {
+		super.onCreate(arg0);
+		setContentView(R.layout.activity_fragment);
+		
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		if (fragment == null) {
+			fragment = createFragment();
+			fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+		}
+	}
+	
+}
