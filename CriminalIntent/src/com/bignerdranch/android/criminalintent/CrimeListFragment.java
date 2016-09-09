ff54ad497f8ed76405799e48e8ee3e9f27436266
Nan Li <crscd@crscdtekiiMac.local>
2016-09-09 10:28:29 +0800

    11.使用ViewPager

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -36,7 +36,7 @@ public class CrimeListFragment extends ListFragment {
 		//Crime c = (Crime)getListAdapter().getItem(position);
 		Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
 		
-		Intent i = new Intent(getActivity(), CrimeActivity.class);
+		Intent i = new Intent(getActivity(), CrimePagerActivity.class);
 		i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
 		startActivity(i);
 	}
